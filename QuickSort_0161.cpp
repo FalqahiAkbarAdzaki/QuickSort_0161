d40a7e1ebda289d6bd34fbe156ac27779c8b5f29
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-03-28 12:05:16 +0700

    commit 15

--- QuickSort_0161.cpp
+++ QuickSort_0161.cpp
@@ -83,4 +83,10 @@ void display() {
 	cout << "\n------------------" << endl;
 	cout << "sorted array" << endl;
 	cout << "------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
 }
