b55d7c30d98b80b911fc8d74b11358ecfeb00bab
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-03-28 11:51:44 +0700

    commit 7

--- QuickSort_0161.cpp
+++ QuickSort_0161.cpp
@@ -49,4 +49,14 @@ void q_short(int low, int high)
 
 
 	while (i <= j)
+	{
+		while ((arr[i] <= pivot) && (i <= high))
+		{
+			i++;
+			cmp_count++;
+		}
+		cmp_count++;
+		while ((arr[j] > pivot) && (j >= low))
+		{ }
+	}
 }
