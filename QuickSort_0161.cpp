2e6409c5f3b737df0a6df318df1b5d2806c21e49
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-03-28 11:14:06 +0700

    commit 2

--- QuickSort_0161.cpp
+++ QuickSort_0161.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int arr[20];
 int cmp count = 0;
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "masukkan panjang elemen array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "Maksimum panjang array adalah 20"
+	}
+}
